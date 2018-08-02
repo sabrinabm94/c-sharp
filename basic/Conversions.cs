77ac81acb20c7f3dab6f98cc78ef4a33ab610be4
Sabrina B <sabrinabgbc@email.com>
2018-08-02 12:43:17 -0300

    Update Conversions.cs

--- basic/Conversions.cs
+++ basic/Conversions.cs
@@ -23,12 +23,16 @@ namespace Conversions
             int mediumNumber = 1300000000; //suporta valores até 32 bits
             long largeNumber = 130000000000; //similar ao inteiro mas com suporte de até 64 bits
             
-            char firstLetter = 's'; //somente aceita aspas simples
+            char firstLetter = 's'; //somente aceita aspas simples, não aceita vazio
             Console.WriteLine(firstLetter);
             
             firstLetter = (char)61; //sinal de =
             Console.WriteLine(firstLetter);
             
+            string firstText = "The year is " + 2018;
+            Console.WriteLine(firstText);
+            string secoundText = "";
+           
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
         }
