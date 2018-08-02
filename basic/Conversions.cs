2416f21d980ad127c324125b7753fed5a47706fa
Sabrina B <sabrinabgbc@email.com>
2018-08-02 12:39:28 -0300

    Update Conversions.cs

--- basic/Conversions.cs
+++ basic/Conversions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,16 +11,25 @@ namespace Conversions
         static void Main(string[] args)
         {
             Console.WriteLine("Starting...");
+            
             double paymentDouble = 1150.00;
 
             int paymentInt;
             paymentInt = (int)paymentDouble; //convertendo double to int
+            
+            Console.WriteLine(paymentInt);
 
             short shortNumber = 150; //suporta valores até 16 bits
             int mediumNumber = 1300000000; //suporta valores até 32 bits
             long largeNumber = 130000000000; //similar ao inteiro mas com suporte de até 64 bits
-
-            Console.WriteLine(paymentInt);
+            
+            char firstLetter = 's'; //somente aceita aspas simples
+            Console.WriteLine(firstLetter);
+            
+            firstLetter = (char)61; //sinal de =
+            Console.WriteLine(firstLetter);
+            
+            Console.WriteLine("Press enter to exit");
             Console.ReadLine();
         }
     }
