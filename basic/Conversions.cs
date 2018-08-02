f1d4c7d23890e1f1cbeb1fa06790c172d946b5d2
Sabrina Boing <sabrinabgbc@email.com>
2018-08-02 08:42:54 -0300

    first

---
+++ basic/Conversions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversions
+{
+    class Conversions
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            double paymentDouble = 1150.00;
+
+            int paymentInt;
+            paymentInt = (int)paymentDouble; //convertendo double to int
+
+            short shortNumber = 150; //suporta valores até 16 bits
+            int mediumNumber = 1300000000; //suporta valores até 32 bits
+            long largeNumber = 130000000000; //similar ao inteiro mas com suporte de até 64 bits
+
+            Console.WriteLine(paymentInt);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ basic/Hello.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Ended, use Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
