02001de52c5261b7b22d34fd29e1d8367c7b073e
Sabrina B <sabrinabgbc@email.com>
2018-08-03 13:18:52 -0300

    Create Program.cs

---
+++ object-oriented/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            new CheckingAccount();
+            Console.ReadLine();
+
+        }
+    }
+}
