73d1834677ffec7c2f411ac9d87ec55e6720a29d
Sabrina B <sabrinabgbc@email.com>
2018-08-28 10:34:55 -0300

    Update 1912-v1.cs

--- uri/1912-v1.cs
+++ uri/1912-v1.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Treinamento
 {
@@ -10,85 +6,101 @@ namespace Treinamento
     {
         static void Main(string[] args)
         {
-            double alturaMaxima = 0.0; //altura a ser descoberta
-            int somaAreaTotal = 0; //soma da área que deve ser igual a A
+            double somaAreaTotal = 0; //soma da área que deve ser igual a A
 
             string primeiraLinha = Console.ReadLine();
             string[] entrada1 = primeiraLinha.Split(' ');
             int N = Convert.ToInt32(entrada1[0]); //quantidade de tiras
-            int A = Convert.ToInt32(entrada1[1]); //soma das áreas da tiras deve ser igual a A
-            int comprimentoTira = 0;
+            double A = Convert.ToInt32(entrada1[1]); //soma das áreas da tiras deve ser igual a A
+            double comprimentoTira = 0;
 
-            int[] vetorAreaBarras = new int[N];
-            string segundaLinha = Console.ReadLine();
-            string[] entrada2 = segundaLinha.Split(' ');
+            if(N != 0 || A != 0)
+            {
+                double[] vetorAreaBarras = new double[N];
+                string segundaLinha = Console.ReadLine();
+                string[] entrada2 = segundaLinha.Split(' ');
 
-            if(N == 0 && A == 0)
-            {
-                //Console.WriteLine("Entrada inválida!");
-            } else
-            {
                 for (int i = 0; i < N; i++)
                 {
-                    comprimentoTira = Convert.ToInt32(entrada2[i]); //comprimento cada tira
+                    // comprimento cada tira
+                    comprimentoTira = Convert.ToInt32(entrada2[i]);
                     vetorAreaBarras[i] = comprimentoTira;
-                    somaAreaTotal = somaAreaTotal + vetorAreaBarras[i]; //soma total de todas as áreas das barras
 
-                    if (vetorAreaBarras[i] > alturaMaxima)
-                    {
-                        alturaMaxima = vetorAreaBarras[i]; //define altura máxima
-                    }
+                    //soma total das áreas das tiras
+                    somaAreaTotal = somaAreaTotal + vetorAreaBarras[i]; 
                 }
+
+                ordenarDecrescente(vetorAreaBarras);
+                //após ordenado descrescente define maior e menor valor
+                double alturaMaxima = vetorAreaBarras[0];
+                double alturaMinima = vetorAreaBarras[vetorAreaBarras.Length-1];
 
                 if (somaAreaTotal == A)
                 {
-                    Console.WriteLine(":D\n"); //área total = área corte.
+                    Console.WriteLine(":D\n"); //área total = área corte
                 }
                 else if (somaAreaTotal < A)
                 {
-                    Console.WriteLine("-.-\n"); //área total menor que a área corte.
+                    Console.WriteLine("-.-\n"); //área total menor que a área corte
+                } else if(somaAreaTotal > A)
+                {                  
+                    //média do corte
+                    double alturaCorte = (alturaMaxima + alturaMinima) / 2.0;
+
+                    //resto do corte
+                    double restoCorte = 0;
+                    double somaAreaTotalNova = 0;
+
+                    for (int i = 0; i < vetorAreaBarras.Length; i++)
+                    {
+                        if (vetorAreaBarras[i] > alturaCorte)
+                        {
+                            restoCorte = restoCorte + (vetorAreaBarras[i] - alturaCorte);
+                            vetorAreaBarras[i] = alturaCorte;
+                        }
+
+                        somaAreaTotalNova = somaAreaTotalNova + vetorAreaBarras[i];
+                    }
+
+                    if(somaAreaTotalNova == somaAreaTotal - A)
+                    {
+
+                        Console.WriteLine(alturaCorte);
+                    }
+
+                    /*
+                     Caso 1: 
+                     5 - 3 - 6 - 2 - 3 = 1
+                     4 - 3 - 4 - 2 - 3 = 16
+                     1 - 0 - 2 - 0 - 0 = 3
+
+                     deve remover 3 de todo o array
+                     deve ver a altura maxima para poder remover 3
+                    */
                 }
-                else
-                {
-                    int aux = A;
-                    Console.WriteLine("\n", BuscaBinaria(aux, vetorAreaBarras, N, 0.0, alturaMaxima)); //buscar o que deve ser cortado
-                }
+            }
+            else
+            {
+                //Console.WriteLine("Valores inválidos!");
             }
 
             Console.WriteLine("Pressione enter para encerrar...");
             Console.ReadLine();
         }
 
-        static double BuscaBinaria(double somaAreaTotal, int[] vetorAreaBarras, int N, double alturaBase, double alturaMaxima)
+        static void ordenarDecrescente(double[] vet)
         {
-            double alturaCorte = (alturaBase + alturaMaxima) / 2.0;
-            double areaCorte = 0.0;
-
-            for (int i = 0; i < N; i++)
+            for (int i = 1; i < vet.Length; i++)
             {
-                if (vetorAreaBarras[i] > alturaCorte) //se a area da barra for menor que a altura do corte
+                for (int j = 0; j < i; j++)
                 {
-                    areaCorte = areaCorte + (vetorAreaBarras[i] - alturaCorte); //define a area para cortar
+                    if (vet[i] > vet[j])
+                    {
+                        double temp = vet[i];
+                        vet[i] = vet[j];
+                        vet[j] = temp;
+                    }
                 }
-            }
-
-            if ((alturaMaxima - alturaBase) < 0.000001)
-            {
-                Console.WriteLine(alturaCorte);
-                return alturaCorte;
-            }
-            else if (areaCorte > somaAreaTotal)
-            {
-                return BuscaBinaria(somaAreaTotal, vetorAreaBarras, N, alturaCorte, alturaMaxima);
-            }
-            else if (areaCorte < somaAreaTotal)
-            {
-                return BuscaBinaria(somaAreaTotal, vetorAreaBarras, N, alturaBase, alturaCorte);
-            }
-            else
-            {
-                Console.WriteLine(alturaCorte);
-                return alturaCorte;
             }
         }
     }
